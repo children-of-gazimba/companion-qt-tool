b8d2e82ed87fe9f2f0d95ccfe4af60f30f3e184c
sebastianWV <stickert@worldviz.com>
2017-12-09 13:57:22 +0100

    fixed playlist random not generating different media indexes on windows

--- src/custom_media_player.cpp
+++ src/custom_media_player.cpp
@@ -98,10 +98,8 @@ void CustomMediaPlayer::setActivation(bool flag)
 
 int CustomMediaPlayer::getRandomIntInRange(int min, int max)
 {
-    std::random_device rd;     // only used once to initialise (seed) engine
-    std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-    std::uniform_int_distribution<int> uni(min,max); // guaranteed unbiased
-    return uni(rng);
+    int range = max - min;
+    return (qrand() % (range+1)) + min;
 }
 
 void CustomMediaPlayer::currentMediaIndexChanged(int position)
