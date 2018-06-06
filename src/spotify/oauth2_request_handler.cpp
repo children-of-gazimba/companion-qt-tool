d9b609e2acf8710b00e0c7d3bf8ad687e67151de
Hagen Hiller <haghiller@gmail.com>
2018-06-06 19:15:12 +0100

    functions containing infos will return a QNetworkReply* to get the request contents where needed

--- src/spotify/oauth2_request_handler.cpp
+++ src/spotify/oauth2_request_handler.cpp
@@ -5,6 +5,8 @@
 
 OAuth2RequestHandler::OAuth2RequestHandler(QObject *parent)
     : QObject(parent)
+    , network_manager_(0)
+    , request_()
     , access_granted_(false)
 {
     network_manager_ = new  QNetworkAccessManager();
@@ -12,39 +14,39 @@ OAuth2RequestHandler::OAuth2RequestHandler(QObject *parent)
             this, &OAuth2RequestHandler::requestFinished);
 }
 
-void OAuth2RequestHandler::get(const QString &url) {
+OAuth2RequestHandler::~OAuth2RequestHandler()
+{
+    network_manager_->deleteLater();
+}
+
+QNetworkReply *OAuth2RequestHandler::get(const QString &url) {
     QUrl u(base_url + url);
     request_.setUrl(u);
-    QNetworkReply *reply  = network_manager_->get(request_);
-    Q_UNUSED(reply);
+    return network_manager_->get(request_);
 }
 
-void OAuth2RequestHandler::put(const QString &url, const QByteArray &parameter) {
+QNetworkReply *OAuth2RequestHandler::put(const QString &url, const QByteArray &parameter) {
     QUrl u(base_url + url);
     request_.setUrl(u);
-    QNetworkReply *reply = network_manager_->put(request_, parameter);
-    Q_UNUSED(reply);
+    return network_manager_->put(request_, parameter);
 }
 
-void OAuth2RequestHandler::put(const QString &url) {
+QNetworkReply *OAuth2RequestHandler::put(const QString &url) {
     QUrl u(base_url + url);
     request_.setUrl(u);
-    QNetworkReply *reply = network_manager_->put(request_, QByteArray());
-    Q_UNUSED(reply);
+    return network_manager_->put(request_, QByteArray());
 }
 
-void OAuth2RequestHandler::post(const QString &url, const QByteArray &parameter) {
+QNetworkReply *OAuth2RequestHandler::post(const QString &url, const QByteArray &parameter) {
     request_.setUrl(base_url + url);
     request_.setUrl(base_url + url);
-    QNetworkReply *reply  = network_manager_->post(request_, parameter);
-    Q_UNUSED(reply);
+    return  network_manager_->post(request_, parameter);
 }
 
-void OAuth2RequestHandler::post(const QString &url) {
+QNetworkReply *OAuth2RequestHandler::post(const QString &url) {
     request_.setUrl(base_url + url);
     request_.setUrl(base_url + url);
-    QNetworkReply *reply = network_manager_->post(request_, QByteArray());
-    Q_UNUSED(reply);
+    return network_manager_->post(request_, QByteArray());
 }
 
 void OAuth2RequestHandler::setRequestHeaders(const QString &token) {
